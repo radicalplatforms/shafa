1189494b27b8e242b7b060bd29bf23f4add68228
Radison Akerman <radison@me.com>
2023-11-09 11:44:26 -0600

    feat: item read, insert, and update is functional

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -3,13 +3,18 @@ import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { version } from "../package.json";
 import { drizzle } from 'drizzle-orm/d1';
+import { eq } from "drizzle-orm";
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { zValidator } from "@hono/zod-validator";
 import { z } from 'zod';
 
-import { item, outfit, item_to_outfit } from "./schema";
+import { item, outfit, item_to_outfit, itemTypeEnum } from "./schema";
 
-const insertItemSchema = createInsertSchema(item);
+const insertItemSchema = createInsertSchema(item, {
+    type: z.nativeEnum(itemTypeEnum),
+    rating: z.number().min(0).max(4),
+    quality: z.number().min(0).max(4)
+});
 const selectItemSchema = createSelectSchema(item);
 
 type Bindings = {
@@ -36,12 +41,24 @@ app.get("/api/items", async (c) => {
   }
 });
 
-app.post("/api/items", zValidator("json", insertItemSchema), async (c) => {
+app.post("/api/items", zValidator("json", insertItemSchema.omit({id: true, timestamp: true})), async (c) => {
     const body = c.req.valid("json");
     console.log(body);
     try {
         const db = drizzle(c.env.DB);
         const results = await db.insert(item).values(body).returning().get();
+        return c.json(results)
+    } catch (e) {
+        return c.json({err: e}, 500)
+    }
+});
+
+app.put("/api/items", zValidator("json", insertItemSchema.omit({timestamp: true})), async (c) => {
+    const body = c.req.valid("json");
+    console.log(body);
+    try {
+        const db = drizzle(c.env.DB);
+        const results = await db.update(item).set(body).where(eq(item.id, body.id)).returning().get();
         return c.json(results)
     } catch (e) {
         return c.json({err: e}, 500)
