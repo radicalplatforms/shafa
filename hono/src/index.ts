946ac544562e0b128afa33f6f70d5593a3f6079c
Radison Akerman <radison@me.com>
2024-01-13 00:12:35 -0600

    [SHAFA-7][SHAFA-4]: added error handler, more work on migrations for tags

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -42,6 +42,11 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.onError((err, c) => {
+    console.error(`${err}`)
+    return c.text('Internal Service Error', 500)
+})
+
 app.use("*", logger());
 app.use("*", prettyJSON());
 
@@ -52,52 +57,44 @@ app.get("/", async (c) => {
 app.get("/api/items", async (c) => {
     const type: number = Number(c.req.query('type') || -1);
     const search: string = c.req.query('search') || "";
-    try {
-        const db = drizzle(c.env.DB, {schema});
-        return c.json(
-            await db.query.items.findMany({
-                where: and(type !== -1 ? eq(items.type, type) : undefined, search !== "" ? or(like(items.name, `%${search}%`), like(items.brand, `%${search}%`)) : undefined),
-                extras: {
-                    lastWorn: sql<string>`(SELECT wear_date
-                                           FROM outfits
-                                           WHERE id = (SELECT outfit_id
-                                                       FROM items_to_outfits
-                                                       WHERE item_id = items.id
-                                                       ORDER BY outfit_id DESC LIMIT 1)
-                                          )`.as("last_worn"),
-                    rollingMonthWears: sql<number>`(SELECT COUNT(DISTINCT (wear_date))
-                                                    FROM outfits
-                                                    WHERE id IN (SELECT outfit_id FROM items_to_outfits WHERE item_id = items.id)
-                                                      AND wear_date >= date ('now'
-                                                        , '-1 month')
-                                                   )`.as("rolling_month_wears"),
-                    rollingYearWears: sql<number>`(SELECT COUNT(DISTINCT (wear_date))
-                                                   FROM outfits
-                                                   WHERE id IN (SELECT outfit_id FROM items_to_outfits WHERE item_id = items.id)
-                                                     AND wear_date >= date ('now'
-                                                       , '-1 year')
-                                                  )`.as("rolling_year_wears")
-                },
-                orderBy: [asc(sql.identifier('last_worn')), desc(items.rating), desc(items.quality)]
-            })
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    const db = drizzle(c.env.DB, {schema});
+    return c.json(
+        await db.query.items.findMany({
+            where: and(type !== -1 ? eq(items.type, type) : undefined, search !== "" ? or(like(items.name, `%${search}%`), like(items.brand, `%${search}%`)) : undefined),
+            extras: {
+                lastWorn: sql<string>`(SELECT wear_date
+                                       FROM outfits
+                                       WHERE id = (SELECT outfit_id
+                                                   FROM items_to_outfits
+                                                   WHERE item_id = items.id
+                                                   ORDER BY outfit_id DESC LIMIT 1)
+                                      )`.as("last_worn"),
+                rollingMonthWears: sql<number>`(SELECT COUNT(DISTINCT (wear_date))
+                                                FROM outfits
+                                                WHERE id IN (SELECT outfit_id FROM items_to_outfits WHERE item_id = items.id)
+                                                  AND wear_date >= date ('now'
+                                                    , '-1 month')
+                                               )`.as("rolling_month_wears"),
+                rollingYearWears: sql<number>`(SELECT COUNT(DISTINCT (wear_date))
+                                               FROM outfits
+                                               WHERE id IN (SELECT outfit_id FROM items_to_outfits WHERE item_id = items.id)
+                                                 AND wear_date >= date ('now'
+                                                   , '-1 year')
+                                              )`.as("rolling_year_wears")
+            },
+            orderBy: [asc(sql.identifier('last_worn')), desc(items.rating), desc(items.quality)]
+        })
+    )
 });
 
 app.post("/api/items", zValidator("json", insertItemSchema.omit({id: true, timestamp: true})), async (c) => {
     const body = c.req.valid("json");
-    try {
-        const db = drizzle(c.env.DB);
-        const author_username = "rak3rman";
-        (body as any).authorUsername = author_username;
-        return c.json(
-            await db.insert(items).values(body).returning()
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    const db = drizzle(c.env.DB);
+    const author_username = "rak3rman";
+    (body as any).authorUsername = author_username;
+    return c.json(
+        await db.insert(items).values(body).returning()
+    )
 });
 
 app.put("/api/items/:id", zValidator("json", insertItemSchema.omit({timestamp: true})), async (c) => {
@@ -105,137 +102,113 @@ app.put("/api/items/:id", zValidator("json", insertItemSchema.omit({timestamp: t
     const body = c.req.valid("json");
     const author_username = "rak3rman";
     (body as any).authorUsername = author_username;
-    try {
-        const db = drizzle(c.env.DB);
-        return c.json(
-            await db.update(items).set(body).where(eq(items.id, id)).returning()
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    const db = drizzle(c.env.DB);
+    return c.json(
+        await db.update(items).set(body).where(eq(items.id, id)).returning()
+    )
 });
 
 app.delete("/api/items/:id", async (c) => {
     const id: number = +c.req.param('id');
-    try {
-        const db = drizzle(c.env.DB);
+    const db = drizzle(c.env.DB);
 
-        await db.delete(itemsToOutfits).where(eq(itemsToOutfits.itemId, id)).run();
+    await db.delete(itemsToOutfits).where(eq(itemsToOutfits.itemId, id)).run();
 
-        return c.json(
-            await db.delete(items).where(eq(items.id, id)).returning()
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    return c.json(
+        await db.delete(items).where(eq(items.id, id)).returning()
+    )
 });
 
 app.get("/api/outfits", async (c) => {
     const rating = c.req.query('rating') as number | undefined;
     const itemIds = c.req.queries('itemId[]') as number[] | [] || [];
-    try {
-        const db = drizzle(c.env.DB, {schema});
-        return c.json(
-            await db.query.outfits.findMany({
-                with: {
-                    itemsToOutfits: {
-                        columns: {
-                            itemId: false,
-                            outfitId: false
-                        },
-                        with: {
-                            item: true
-                        },
-                        orderBy: (itemsToOutfits, {asc}) => [asc(itemsToOutfits.type)]
-                    }
-                },
-                orderBy: (outfits, {desc}) => [desc(outfits.wearDate)],
-                where: and(
-                    itemIds.length > 0 ? inArray(outfits.id, (
-                        await db.select()
-                            .from(itemsToOutfits)
-                            .where(inArray(itemsToOutfits.itemId, itemIds))
-                            .groupBy(itemsToOutfits.outfitId)
-                            .having(sql`count(distinct item_id) =
-                            ${itemIds.length}`)
-                            .all()
-                    ).map(e => e.outfitId)) : undefined,
-                    rating !== undefined ? eq(outfits.rating, rating) : undefined
-                )
-            })
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    const db = drizzle(c.env.DB, {schema});
+    return c.json(
+        await db.query.outfits.findMany({
+            with: {
+                itemsToOutfits: {
+                    columns: {
+                        itemId: false,
+                        outfitId: false
+                    },
+                    with: {
+                        item: true
+                    },
+                    orderBy: (itemsToOutfits, {asc}) => [asc(itemsToOutfits.type)]
+                }
+            },
+            orderBy: (outfits, {desc}) => [desc(outfits.wearDate)],
+            where: and(
+                itemIds.length > 0 ? inArray(outfits.id, (
+                    await db.select()
+                        .from(itemsToOutfits)
+                        .where(inArray(itemsToOutfits.itemId, itemIds))
+                        .groupBy(itemsToOutfits.outfitId)
+                        .having(sql`count(distinct item_id) =
+                        ${itemIds.length}`)
+                        .all()
+                ).map(e => e.outfitId)) : undefined,
+                rating !== undefined ? eq(outfits.rating, rating) : undefined
+            )
+        })
+    )
 });
 
 app.post("/api/outfits", zValidator("json", insertOutfitSchema), async (c) => {
     const body = c.req.valid("json");
-    try {
-        const db = drizzle(c.env.DB);
-        const author_username = "rak3rman";
-        (body as any).authorUsername = author_username;
+    const db = drizzle(c.env.DB);
+    const author_username = "rak3rman";
+    (body as any).authorUsername = author_username;
 
-        const itemIdsTypes = body.itemIdsTypes
-        delete body.itemIdsTypes
+    const itemIdsTypes = body.itemIdsTypes
+    delete body.itemIdsTypes
 
-        const newOutfit = await db.insert(outfits).values(body).returning({id: outfits.id}).get();
+    const newOutfit = await db.insert(outfits).values(body).returning({id: outfits.id}).get();
 
-        if (itemIdsTypes !== undefined) {
-            await db.insert(itemsToOutfits).values(itemIdsTypes.map(e => ({
-                itemId: e.id,
-                outfitId: newOutfit.id,
-                type: e.type
-            }))).run();
-        }
-
-        return c.json(newOutfit)
-    } catch (e) {
-        return c.json(e, 500)
+    if (itemIdsTypes !== undefined) {
+        await db.insert(itemsToOutfits).values(itemIdsTypes.map(e => ({
+            itemId: e.id,
+            outfitId: newOutfit.id,
+            type: e.type
+        }))).run();
     }
+
+    return c.json(newOutfit)
 });
 
 app.put("/api/outfits/:id", zValidator("json", insertOutfitSchema), async (c) => {
     const id: number = +c.req.param('id');
     const body = c.req.valid("json");
-    try {
-        const db = drizzle(c.env.DB);
-        const author_username = "rak3rman";
-        (body as any).authorUsername = author_username;
+    const db = drizzle(c.env.DB);
+    const author_username = "rak3rman";
+    (body as any).authorUsername = author_username;
 
-        const itemIdsTypes = body.itemIdsTypes
-        delete body.itemIdsTypes
+    const itemIdsTypes = body.itemIdsTypes
+    delete body.itemIdsTypes
 
-        if (itemIdsTypes !== undefined) {
-            await db.delete(itemsToOutfits).where(eq(itemsToOutfits.outfitId, id)).run();
-            await db.insert(itemsToOutfits).values(itemIdsTypes.map(e => ({
-                itemId: e.id,
-                outfitId: id,
-                type: e.type
-            }))).run();
-        }
-
-        return c.json(
-            await db.update(outfits).set(body).where(eq(outfits.id, id)).returning()
-        )
-    } catch (e) {
-        return c.json(e, 500)
+    if (itemIdsTypes !== undefined) {
+        await db.delete(itemsToOutfits).where(eq(itemsToOutfits.outfitId, id)).run();
+        await db.insert(itemsToOutfits).values(itemIdsTypes.map(e => ({
+            itemId: e.id,
+            outfitId: id,
+            type: e.type
+        }))).run();
     }
+
+    return c.json(
+        await db.update(outfits).set(body).where(eq(outfits.id, id)).returning()
+    )
 });
 
 app.delete("/api/outfits/:id", async (c) => {
     const id: number = +c.req.param('id');
-    try {
-        const db = drizzle(c.env.DB);
+    const db = drizzle(c.env.DB);
 
-        await db.delete(itemsToOutfits).where(eq(itemsToOutfits.outfitId, id)).run();
+    await db.delete(itemsToOutfits).where(eq(itemsToOutfits.outfitId, id)).run();
 
-        return c.json(
-            await db.delete(outfits).where(eq(outfits.id, id)).returning()
-        )
-    } catch (e) {
-        return c.json(e, 500)
-    }
+    return c.json(
+        await db.delete(outfits).where(eq(outfits.id, id)).returning()
+    )
 });
 
 export default app;
