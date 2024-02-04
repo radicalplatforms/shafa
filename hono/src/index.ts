2be607e87d2a1b43bf6aa527d0aa004f03b9ab54
Vincent Do <wavyfingers@gmail.com>
2024-02-04 16:56:09 -0600

    remove repeated middleware code

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -18,16 +18,6 @@ app.onError((err, c) => {
 app.use('*', logger());
 app.use('*', prettyJSON());
 
-app.use('*', async (c, next) => {
-  if (!c.env) {
-    throw new Error('Environment variable is not available');
-  }
-  const dbConfig = c.env!.DB as D1Database;
-  const db = drizzle(dbConfig, { schema });
-  c.set('db', db);
-  await next();
-});
-
 app.get('/', async (c) => {
   return c.text(`Shafa API v${version}`);
 });
