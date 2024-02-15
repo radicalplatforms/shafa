ef823835bcb12beb946d2603da3bb2470dc727a9
Vincent Do <85699604+vincentdo1@users.noreply.github.com>
2024-02-14 20:33:25 -0600

    test: implement mock database (#47)
    * test: initial mock db using jest
    
    * test: push current changes for now
    
    * test: mock database functionality and test case passes
    
    * test: use unstable_dev instead of miniflare
    
    * test: create local test db with tests
    
    * test: better naming unit test
    
    * chore: wrap npm run test with wrangler action
    
    * chore: remove duplicate uses in npm run test
    
    * chore: remove run and replace with script
    
    * chore: separate npm run test
    
    * chore: separate test migration and jest test command
    
    * chore: attempt fetch error fix hono integration
    
    * chore: fix formatting issue
    
    * chore: revert schema change
    
    * chore: remove unsed import
    
    * chore: address comments
    
    * chore: reorganize github action for test db migration
    
    * chore: address comments
    
    * chore: util -> utils

---
+++ hono/test/factory/items.ts
@@ -0,0 +1,11 @@
+export function validItem() {
+  return {
+    name: 'Vintage Denim Pants',
+    brand: 'Levi',
+    photo: 'https://example.com/',
+    type: 0,
+    rating: 2,
+    quality: 4,
+    authorUsername: 'rak3rman',
+  }
+}
--- hono/test/items.test.ts
+++ hono/test/items.test.ts
@@ -1,16 +1,43 @@
-// import app from '../src/index';
-//
-// const MOCK_ENV = {
-//  DB: {
-//    prepare: () => {
-//      // Mocked D1
-//    },
-//  },
-// };
+import { execSync } from 'child_process'
+import type { UnstableDevWorker } from 'wrangler'
+import { unstable_dev } from 'wrangler'
+import type { items } from '../src/schema'
+import { validItem } from './factory/items'
 
-describe('GET /items', () => {
-  it('should return no items', async () => {
-    // const res = await app.request('/items', {}, MOCK_ENV);
-    // expect(await res.json()).toEqual([])
+describe('Items Unit Test', () => {
+  let worker: UnstableDevWorker
+
+  beforeAll(async () => {
+    worker = await unstable_dev('./src/index.ts', {
+      env: 'test',
+      experimental: {
+        disableExperimentalWarning: true,
+      },
+      ip: '127.0.0.1',
+      persistTo: './test/tmp-d1-db',
+    })
+  })
+
+  afterAll(async () => {
+    execSync(`npm run wrtest-clean-db-local`)
+    await worker.stop()
+  })
+
+  test('GET /items: should return no items', async () => {
+    const res = await worker.fetch('/api/items')
+    expect(res.status).toBe(200)
+    expect(await res.json()).toEqual([])
+  })
+
+  test('POST /items: should create and return one item', async () => {
+    const res = await worker.fetch('/api/items', {
+      method: 'POST',
+      body: JSON.stringify(validItem()),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const json = (await res.json()) as (typeof items)[]
+    expect(res.status).toBe(200)
+    expect(json).toMatchObject([validItem()])
+    expect(json[0].id).toEqual(1)
   })
 })
