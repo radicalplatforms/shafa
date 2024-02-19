6bd270bc0d4c3fd44faa71dafd9e9fdf54eeb3e6
Radison Akerman <radison@me.com>
2024-02-19 11:44:01 -0600

    fix: modified try catch for pg-mem

--- hono/src/utils/__mocks__/injectDB.ts
+++ hono/src/utils/__mocks__/injectDB.ts
@@ -16,27 +16,17 @@ export default async function injectDB(c: Context, next: Function) {
   const db = drizzle(
     async (sql, params, method) => {
       const sqlBody = sql.replace(/;/g, '')
-      try {
-        console.log('M1-3')
-        const result = await client.query({
-          text: sqlBody,
-          values: params,
-          rowMode: method === 'all' ? 'array' : undefined,
-        })
-        console.log('M2-3')
-        console.log(result)
-        return { rows: result.rows }
-      } catch (e: any) {
-        console.error('Error from pg proxy server: ', e.response.data)
-        return { rows: [] }
-      }
+      const result = await client.query({
+        text: sqlBody,
+        values: params,
+        rowMode: method === 'all' ? 'array' : undefined,
+      })
+      return { rows: result.rows }
     },
     { schema }
   )
   c.set('db', db)
   await client.connect()
-  console.log('M2')
   await next()
-  console.log('M3')
   await client.end()
 }
